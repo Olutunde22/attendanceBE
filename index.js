0a361a273bd4c5455851586b2068f761506be252
Olutunde <solabi.tunde@gmail.com>
2022-01-03 17:22:16 +0000

    bug

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ mongoose.connect(
   }
 )
 
-app.listen(process.env.SERVER_PORT, () =>
-console.log('Server listening on port ' + process.env.SERVER_PORT)
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Server listening on port ' + process.env.PORT)
 )
