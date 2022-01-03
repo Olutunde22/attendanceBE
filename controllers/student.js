6a9b1dbb53cd513cabc2e77390f892318118ca1c
Olutunde <solabi.tunde@gmail.com>
2022-01-03 19:58:34 +0000

    added confirmation for adding student

--- controllers/student.js
+++ controllers/student.js
@@ -4,6 +4,8 @@ const addStudent = async (req, res) => {
     const student = await Student.addStudent({ ...req.body })
     if (student[0] === false) {
         return res.status(400).json({ message: 'Matric Number already Exists' })
+    }else{
+        return res.status(200).json({message: 'Success'})
     }
 }
 
